fe65018d3486de9f3fd6f7f315f138dd36b4583e
yujinKim <yujinkimvv@gmail.com>
2025-06-08 14:08:41 +0900

    feat: Timetable entity에 누락 필드 추가

--- seurasaeng_be/src/main/java/onehajo/seurasaeng/entity/Timetable.java
+++ seurasaeng_be/src/main/java/onehajo/seurasaeng/entity/Timetable.java
@@ -35,6 +35,10 @@ public class Timetable {
     @Column(name = "boarding_location", columnDefinition = "VARCHAR(100)")
     private String boardingLocation;
 
+    @NotNull
+    @Column(name = "dropoff_location", columnDefinition = "VARCHAR(100)")
+    private String dropoffLocation;
+
     @Column(name = "arrival_minutes", columnDefinition = "INTEGER")
     private Integer arrivalMinutes;
 
