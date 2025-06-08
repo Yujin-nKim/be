0c28abaf87a939d6336a326e2614e2bf53879897
yujinKim <yujinkimvv@gmail.com>
2025-06-08 14:11:04 +0900

    feat: 시간표 목록 조회 API 구현

---
+++ seurasaeng_be/src/main/java/onehajo/seurasaeng/shuttle/repository/TimetableRepository.java
@@ -0,0 +1,12 @@
+package onehajo.seurasaeng.shuttle.repository;
+
+import onehajo.seurasaeng.entity.Shuttle;
+import onehajo.seurasaeng.entity.Timetable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TimetableRepository extends JpaRepository<Timetable, Long> {
+    List<Timetable> findByShuttleOrderByDepartureTimeAsc(Shuttle shuttle);
+
+}
